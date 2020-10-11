09a8f5c4e28a2e59941db32b02d230990c1c10f7
Derrick Bol <derrickbol33@gmail.com>
2020-10-12 00:42:44 +0800

    Solved the homework and all tests pass

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,37 @@
+# frozen_string_literal: true
+
+require 'csv'
+
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
+    source = CSV.parse(tsv, col_sep: "\t")
 
+    source_headings = source[0]
+    source_data = source.slice(1, source.length - 1)
+
+    @data = source_data.map do |current_row|
+      source_headings.each_with_index.reduce({}) do |accumulator, current_heading_with_index|
+        heading, index = current_heading_with_index
+
+        accumulator.merge({ heading => current_row[index] })
+      end
+    end
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
+    attributes = @data[0].keys.map(&:to_s)
 
+    CSV.generate(headers: true, col_sep: "\t") do |csv|
+      csv << attributes
+
+      @data.each do |row|
+        csv << attributes.map { |attr| row[attr] }
+      end
+    end
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,20 @@
-# Module that can be included (mixin) to take and output Yaml data
+# frozen_string_literal: true
+
+require 'yaml'
+
+# Module that can be included (mixin) to take and output yaml data
+module YamlBuddy
+  # take_yaml: converts a String with yaml data into @data
+  # parameter: yaml - a String in yaml format
+  def take_yaml(yaml)
+    return unless yaml
+
+    @data = YAML.parse(yaml).to_ruby
+  end
+
+  # to_yaml: converts @data into yaml string
+  # returns: String in yaml format
+  def to_yaml
+    @data.to_yaml
+  end
+end
